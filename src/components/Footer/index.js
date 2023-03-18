04f8065404b207158e8520c92740173c21ed8855
HoldyClaus <holdenclaus0212@gmail.com>
2023-03-18 01:37:51 -0500

    changes to footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/HoldyClaus"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/github-logo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/holden-claus-656559139/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/fdgsgfdfdg"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/twitter-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
